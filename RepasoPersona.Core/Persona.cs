1e55237350c70240137f497c4b3f219ccaed631a
ArturoCruz606 <ar7ur0321@gmail.com>
2022-03-09 15:40:13 -0300

    Creacion Repaso
    jsjs

---
+++ RepasoPersona.Core/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RepasoPersona.Core
+{
+    public class Persona
+    {
+        public string Nombre { get; private set; }
+        public string Apellido { get; private set; }
+        public double Efectivo { get; private set; }
+        public Persona() => Efectivo = 0;
+        public Persona(string nombre, string apellido, double efectivo){
+            Nombre = nombre;
+            Apellido = apellido;
+            Efectivo = efectivo;
+        }
+        public void Debitar(double monto){
+            if (Efectivo < monto)
+                throw new Exception("Saldo Insuficiente");
+            if (monto <= 0)
+                throw new Exception("Se esperaba monto mayor a cero");
+            Efectivo -= monto;
+        }
+        public void Acreditar(double monto){
+            if (monto <= 0)
+                throw new Exception("Se esperaba monto mayor a cero");
+            Efectivo += monto;
+        }
+    }
+}
