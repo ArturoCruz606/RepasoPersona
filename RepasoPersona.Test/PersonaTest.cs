1f4d9afb8ef03910310e08d56b33f9edf780d079
luchoxx87 <ar7ur0321@gmail.com>
2022-03-10 15:44:46 -0300

    Continuando Proyecto de Test
    jsjs

--- RepasoPersona.Test/PersonaTest.cs
+++ RepasoPersona.Test/PersonaTest.cs
@@ -1,14 +1,19 @@
 using System;
 using Xunit;
+using RepasoPersona.Core;
 
 namespace RepasoPersona.Test
 {
     public class PersonaTest
     {
+        public Persona ElAndres { get; set; }
         [Fact]
-        public void Test1()
+        public void Constructor()
         {
-
+            ElAndres = new Persona("Pepito", "Gomez", 0);
+            Assert.Equal("Pepito", ElAndres.Nombre);
+            Assert.Equal("Gomez", ElAndres.Apellido);
+            Assert.Equal(0, ElAndres.Efectivo);
         }
     }
 }
