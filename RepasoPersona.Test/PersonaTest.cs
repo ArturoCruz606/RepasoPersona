c8bd6caafdfa106570f687b5b965914317e67841
luchoxx87 <ar7ur0321@gmail.com>
2022-03-16 14:07:22 -0300

    Añadido de AcreditarPositivo

--- RepasoPersona.Test/PersonaTest.cs
+++ RepasoPersona.Test/PersonaTest.cs
@@ -7,13 +7,19 @@ namespace RepasoPersona.Test
     public class PersonaTest
     {
         public Persona ElAndres { get; set; }
+        public PersonaTest() => ElAndres = new Persona("Pepito", "Gomez", 0);
         [Fact]
         public void Constructor()
         {
-            ElAndres = new Persona("Pepito", "Gomez", 0);
             Assert.Equal("Pepito", ElAndres.Nombre);
             Assert.Equal("Gomez", ElAndres.Apellido);
             Assert.Equal(0, ElAndres.Efectivo);
+        }
+        [Fact]
+        public void AcreditarPositivo(){
+            double esperado = 1000;
+            ElAndres.Acreditar(esperado);
+            Assert.Equal(esperado, ElAndres.Efectivo, 3);
         }
     }
 }
