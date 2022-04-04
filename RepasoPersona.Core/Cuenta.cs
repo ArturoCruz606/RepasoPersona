85c31fa97a62b78e4b0dab2ebdebc2580e3ec9d8
luchoxx87 <ar7ur0321@gmail.com>
2022-04-04 16:19:36 -0300

    Finalizacion de CuentaTest
    jajas

--- RepasoPersona.Core/Cuenta.cs
+++ RepasoPersona.Core/Cuenta.cs
@@ -5,8 +5,10 @@ namespace RepasoPersona.Core
         public int CBU { get; set; }
         public Persona Cliente { get; set; }
         public Cuenta() => Saldo = 0;
-        public Cuenta(int cbu, Persona cliente){
-
+        public Cuenta(int cbu, Persona cliente, int saldo){
+            CBU = cbu;
+            Cliente = cliente;
+            Saldo = saldo;
         } 
     }
 }
--- RepasoPersona.Test/CuentaTest.cs
+++ RepasoPersona.Test/CuentaTest.cs
@@ -7,14 +7,50 @@ namespace RepasoPersona.Test
     public class CuentaTest
     {
         public Cuenta Paypal { get; set; }
-        public PersonaTest() => Paypal = new Persona(100,"Juan", 0);
+        public CuentaTest() => Paypal = new Cuenta(100,null, 0);
 
         [Fact]
         public void Constructor()
         {
             Assert.Equal(100, Paypal.CBU);
-            Assert.Equal("Juan", Paypal.Cliente.Nombre);
+            Assert.Equal(null, Paypal.Cliente);
             Assert.Equal(0, Paypal.Saldo);
+        }
+
+        [Fact]
+        public void AcreditarPositivo()
+        {
+            double esperado = 1000;
+            Paypal.Acreditar(esperado);
+            
+            Assert.Equal(esperado, Paypal.Saldo, 3);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-157.34)]
+        public void AcreditarCeroONegativo(double monto)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => Paypal.Acreditar(0));
+            Assert.Equal("El monto tiene que ser mayor a cero.", ex.Message);
+        }
+
+        [Fact]
+        public void Debitar()
+        {
+            double monto = 500.45;
+            double debito = 135.45;
+            Paypal.Acreditar(monto);
+            Paypal.Debitar(debito);
+
+            Assert.Equal(monto - debito, Paypal.Saldo, 2);
+        }
+
+        [Fact]
+        public void DebitarInsuficiente()
+        {
+            var ex = Assert.Throws<InvalidOperationException>(() => Paypal.Debitar(1000));
+            Assert.Equal("El monto supera al saldo de la cuenta.", ex.Message);
         }
     }
 }
